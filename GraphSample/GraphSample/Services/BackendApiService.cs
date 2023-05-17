76ccd33865b3d412fdfe418d2d274f40cd1c7afe
konstantinos Sougiou <94688359+konsougiou@users.noreply.github.com>
2023-05-17 14:52:30 +0100

    for reference

--- GraphSample/GraphSample/Services/BackendApiService.cs
+++ GraphSample/GraphSample/Services/BackendApiService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text.Json;
+using System.Net.Http.Json;
+using System.Linq;
 using SharedModels.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
@@ -15,7 +17,7 @@ namespace GraphSample.Services
         public BackendApiService(HttpClient httpClient)
 		{
 			this.httpClient = httpClient;
-		}
+        }
 
 		public async Task<List<ApplicationTimeline>> getUserTimelinesAsync(string username)
 		{
@@ -26,6 +28,29 @@ namespace GraphSample.Services
             List<ApplicationTimeline> timelines = BsonSerializer.Deserialize<List<ApplicationTimeline>>(responseBody);
 			return timelines;
         }
+
+        public async Task<ApplicationTimeline> getUserTimelineAsync(string username, int timelineID)
+        {
+            var response = await httpClient.GetAsync($"https://localhost:7023/api/JobApplicants/get-timeline/{username}/{timelineID}");
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // Deserialize the response body into your custom model
+            ApplicationTimeline timelines = BsonSerializer.Deserialize<ApplicationTimeline>(responseBody);
+            return timelines;
+        }
+
+
+        public async Task<bool> removeEmail(string email, string username, int timelineID)
+		{
+			var selectedEmail = new EmailBson { emailAddress = email, timelineID = timelineID };
+
+            var response = await httpClient.PostAsJsonAsync($"https://localhost:7023/api/JobApplicants/remove-email/{username}", selectedEmail);
+
+            return response.IsSuccessStatusCode;
+
+        }
+
+
 	}
 }
 
--- GraphSample/GraphSample/Services/IBackendApiService.cs
+++ GraphSample/GraphSample/Services/IBackendApiService.cs
@@ -6,6 +6,8 @@ namespace GraphSample.Services
 	public interface IBackendApiService
 	{
         public Task<List<ApplicationTimeline>> getUserTimelinesAsync(string username);
+        public Task<ApplicationTimeline> getUserTimelineAsync(string username, int timelineID);
+        public Task<bool> removeEmail(string email, string username, int timelineID);
 
     }
 }
