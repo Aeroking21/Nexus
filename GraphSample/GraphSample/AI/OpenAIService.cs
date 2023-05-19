a3ac0a82e409b58ff7626d0455442f9cac60a62c
Angus Parry <angus.parry20@imperial.ac.uk>
2023-05-19 11:56:29 +0100

    AI started

---
+++ GraphSample/GraphSample/AI/OpenAIService.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.Options;
+using static System.Environment;
+
+namespace GraphSample.AI
+{
+    public class OpenAIService
+    {
+        private readonly OpenAIClient client;
+        public readonly string engine = "text-davinci-003";
+        public readonly string endpoint = "https://plannerai.openai.azure.com/";
+        public readonly string key = "63448da86da048d49845249803372807";
+
+        public OpenAIService()
+        {
+            client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+        }
+    }
+}
