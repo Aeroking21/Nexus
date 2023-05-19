fdaa7cee27a53b6da4dc315c143aa9ed95cf57db
Angus Parry <angus.parry20@imperial.ac.uk>
2023-05-19 17:29:40 +0100

    AI Summary works with txt files
    Still need to work on extracting text from pdf or similar files as getting metadata at the moment

--- GraphSample/GraphSample/AI/OpenAIService.cs
+++ GraphSample/GraphSample/AI/OpenAIService.cs
@@ -7,14 +7,11 @@ namespace GraphSample.AI
 {
     public class OpenAIService
     {
-        private readonly OpenAIClient client;
-        public readonly string engine = "text-davinci-003";
+        public readonly string engine = "PlannerOpenAI";
         public readonly string endpoint = "https://plannerai.openai.azure.com/";
         public readonly string key = "63448da86da048d49845249803372807";
 
-        public OpenAIService()
-        {
-            client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
-        }
+        public readonly OpenAIClient client = new OpenAIClient(new Uri("https://plannerai.openai.azure.com/"), new AzureKeyCredential("63448da86da048d49845249803372807"));
+        
     }
 }
